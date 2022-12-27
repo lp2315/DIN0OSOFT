f7330c60a4db8aa6e84cf02ff3bda5041c325f4d
din0o <a05iaojfw@mozmail.com>
2022-12-27 15:56:57 +0100

    Update dino_primefact.py

--- dino_primefact.py
+++ dino_primefact.py
@@ -10,24 +10,32 @@ primes = [x for x in range(2, 10000) if all(x % y != 0 for y in range(3, x))]
 def prime_fact(n):
     n_primes = []  # LIST OF PRIME DIVISORS
 
-    for i in primes:  # ITERATE OVER PRIMES
+    if n in primes:
+        return print(str(n) + " is a prime number!")
 
-        while n % i == 0:  # WHILE n IS DIVISIBLE BY NEXT PRIME i
-            n_primes.append(i)  # ADD i TO LIST
-            n = n / i  # MOVE ON TO NEXT PRIME i
 
     if n > 1:
-        n_primes.append(int(n))  # IF n IS A POSITIVE INTEGER, ADD TO PRIME LIST
+        for i in primes:  # ITERATE OVER PRIMES
 
-    return print(n_primes)  # PRINT LIST
+            while n % i == 0:  # WHILE n IS DIVISIBLE BY NEXT PRIME i
+                n_primes.append(i)  # ADD i TO LIST
+                n = n / i  # MOVE ON TO NEXT PRIME i
+
+        n_primes.append(int(n))  # IF n IS A POSITIVE INTEGER, ADD TO PRIME LIST
+        return n_primes.remove(1), print(n_primes)
+
+    else:
+        return print("Integer must be higher than 1!")
 
 
 #
 
 # USER INPUT
 
-x = True
-
-while x:  # REPEAT PROMPT INDEFINITELY
+while True:
     x = input("Number?\n")
-    prime_fact(int(x))
+    try:
+        x = int(x)
+        prime_fact(x)
+    except ValueError:
+        print("Number must be an integer!")
