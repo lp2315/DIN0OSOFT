ffa8545a73cc73e1d10ddd1775d7d79e1a72eff7
din0o <a05iaojfw@mozmail.com>
2024-10-16 09:27:00 +0200

    Update keyboard_lock_toggler.py

--- keyboard_lock_toggler.py
+++ keyboard_lock_toggler.py
@@ -8,12 +8,12 @@ Checks if button_lock is activated
 def is_lock_active(button_lock) -> bool:
     # caps lock 0x14
     if button_lock == "caps":
-        return ctypes.windll.user32.GetKeyState(0x14) & 0x0001 != 0 and print('caps on')
+        return ctypes.windll.user32.GetKeyState(0x14) & 0x0001 != 0
 
     # num lock 0x90
     if button_lock == "num":
-        return ctypes.windll.user32.GetKeyState(0x90) & 0x0001 != 0 and print('num on')
+        return ctypes.windll.user32.GetKeyState(0x90) & 0x0001 != 0
 
     # scroll lock 0x91
     if button_lock == "scroll":
-        return ctypes.windll.user32.GetKeyState(0x91) & 0x0001 != 0 and print('scroll on')
+        return ctypes.windll.user32.GetKeyState(0x91) & 0x0001 != 0
